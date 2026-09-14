f42ce15a955f9f7eefb25134be9a5deb3947e5c2
HuemersonFMG <huemersonsuper@gmail.com>
2026-09-13 23:29:59 -0300

    Adiciona projeto BaseAppsSankhya

---
+++ BaseAppsSankhya/README.md
@@ -0,0 +1,36 @@
+﻿# Base Apps Sankhya
+
+## Base para apps utilizando as APIs do Sankhya
+
+>Esse projeto serve de base para apps Android com versão 9 ou superior, ele vem com funcionalidade básica utilizando login e senha criptografada, é totalmente desenvolvido em Kotlin conforme orientado pela Google.
+>
+>Para funcionamento básico, deve ser informado o servidor do Sankhya e deve ser criada uma View de nome AD_VIEWUSUARIO com os campos CODUSU e NOMEUSU da TSIUSU, ou o código pode ser modificado para que atenda as necessidades de cada empresa.
+
+### Funcionalidades
+
+>Para uma completa funcionalidade do app, utilizo a biblioteca do Retrofit2 para conexão com o servidor e coleta dos dados em formato json, o Room para salvar alguns dados de maneira local, o Hilt para injeção de dependência e o Picasso para manipulação de imagens, além das bibliotecas padrões do Android.
+
+
+<table><tr>
+<td><img src="https://github.com/lucasfpastre/ImageRepository/blob/main/Base/Base_Tela_Login.png" width=300 alt="Imagem da tela inicial do aplicativo exibindo usuário, senha, botão para configurar o servidor e uma chave para gravar o usuário ou não" title="Tela de Login" border=/></td>
+<td><img src="https://github.com/lucasfpastre/ImageRepository/blob/main/Base/Base_Tela_Inicial_Teste.png" width=300 alt="Tela inicial exibindo o usuário Teste" title="Tela Inicial"/></td>
+<td><img src="https://github.com/lucasfpastre/ImageRepository/blob/main/Base/Base_Tela_Inicial_Lucas.png" width=300 alt="Tela inicial exibindo o usuário Lucas Pastre" title="Tela Inicial"/></td>
+<td><img src="https://github.com/lucasfpastre/ImageRepository/blob/main/Base/Base_Tela_Inicial_Menu.png" width=300 alt="Tela inicial exibindo o menu com opções: Home, Gallery e Slideshow, e uma foto do usuário conectado, Lucas Pastre e sua foto" title="Menu do App"/></td>
+<td><img src="https://github.com/lucasfpastre/ImageRepository/blob/main/Base/Base_Tela_Configurações.png" width=300 alt="Tela de configurações com um botão de voltar" title="Tela de Configurações"/></td>
+</tr></table>
+
+### Links para as Bibliotecas
+
+>[Retrofit2](https://square.github.io/retrofit/)
+>
+>[Room](https://developer.android.com/training/data-storage/room)
+>
+>[Hilt](https://dagger.dev/hilt/gradle-setup)
+>
+>[Picasso](https://github.com/square/picasso)
+>
+>[Coroutines](https://developer.android.com/kotlin/coroutines?hl=pt-br)
+>
+>[LifeCycle](https://developer.android.com/topic/libraries/architecture/lifecycle)
+
+Esse código não é um requisito para criação de apps, ele é uma base para quem tem interesse em fazer e não tem ideia de como começar.
---
+++ BaseAppsSankhya/app/src/main/java/br/com/generic/base/ui/fragments/home/HomeViewModel.kt
@@ -0,0 +1,172 @@
+package br.com.generic.base.ui.fragments.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import br.com.generic.base.data.extensions.countMatches
+import br.com.generic.base.data.repository.RequestRepository
+import br.com.generic.base.models.procedure.request.ProcedureBody
+import br.com.generic.base.models.procedure.response.ProcedureResponse
+import br.com.generic.base.models.server.ServerData
+import br.com.generic.base.models.service.request.ServiceRequest
+import br.com.generic.base.models.service.response.JSessionID
+import br.com.generic.base.models.service.response.LoginResponse
+import br.com.generic.base.models.service.response.ResponseBody
+import br.com.generic.base.models.view.request.ViewRequest
+import br.com.generic.base.models.view.response.generic.GenericResponseNames
+import br.com.generic.base.models.view.response.generic.multi.MultiResponseBody
+import br.com.generic.base.models.view.response.generic.single.SingleResponseBody
+import com.google.gson.Gson
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(private val requestRepo : RequestRepository) : ViewModel() {
+
+    val serverData : LiveData<ServerData> = requestRepo.server.getServer().asLiveData()
+    val loginResponse = MutableLiveData<LoginResponse?>()
+    val responseBody = MutableLiveData<ResponseBody?>()
+    val sessionId = MutableLiveData<JSessionID?>()
+    val timerStatus = MutableLiveData(false)
+    val logoutStatus = MutableLiveData(false)
+    val requestStatus = MutableLiveData(false)
+    val connectionFailure = MutableLiveData(false)
+    val serverStatus = MutableLiveData(false)
+    val connectionStatusDown = MutableLiveData(false)
+    val fragmentViewLoaded = MutableLiveData(false)
+    val procedureReturn = MutableLiveData(false)
+    val procedureError = MutableLiveData(false)
+    val procedureResponse = MutableLiveData<ProcedureResponse>()
+    val multiViewResponseBody = MutableLiveData<MultiResponseBody>() // Aqui pode ser trocado de tela para tela
+    val singleViewResponseBody = MutableLiveData<SingleResponseBody>() // Aqui pode ser trocado de tela para tela
+    var viewArray = ArrayList<GenericResponseNames?>() // Aqui pode ser trocado de tela para tela
+    var cookieList : List<String?> = ArrayList()
+    var responseData : String = ""
+    var failureMessage : String = ""
+    var timerJob: Job? = null
+
+    // Requisição de Login
+    fun getSessionId(serviceRequest: ServiceRequest, newUrl: String) {
+        requestRepo.remote.getSessionId(serviceRequest, newUrl).enqueue(object : Callback<Any> {
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                val gson = Gson()
+                if (response.body().toString().contains("jsessionid")) {
+                    cookieList = response.headers().values("Set-Cookie")
+                    loginResponse.value = gson.fromJson(gson.toJson(response.body()), LoginResponse::class.java)
+                    responseBody.value = loginResponse.value?.responseBody
+                    sessionId.value = responseBody.value?.jsessionid
+                    responseData = sessionId.value.toString()
+                    requestStatus.value = true
+                    timerJob?.cancel()
+                } else {
+                    connectionFailure.value = true
+                    failureMessage = "Usuário ou senha inválidos"
+                }
+            }
+
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                failureMessage = t.toString()
+                connectionFailure.value = true
+            }
+        })
+    }
+
+    // Chamada assíncrona da view
+    fun getViewData(viewRequest: ViewRequest, newUrl: String, cookie: String) {
+        requestRepo.remote.getViewData(viewRequest, newUrl, cookie).enqueue(object : Callback<Any> {
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                if (response.body().toString().contains("Não autorizado")) { // Erro em caso de tempo limite da chave
+                    connectionStatusDown.value = true
+                } else {
+                    timerJob?.cancel()
+                    val gson = Gson()
+                    val counter = countMatches(response.body().toString(), "CODUSU") // Verifico se a view retornou um ou mais resultados e trato de forma adequada
+                    if (counter > 1) {
+                        multiViewResponseBody.value = gson.fromJson(gson.toJson(response.body()), MultiResponseBody::class.java)
+                        viewArray = multiViewResponseBody.value?.responseBody?.records?.viewNames!!
+                        fragmentViewLoaded.value = true
+                    } else {
+                        viewArray = ArrayList() // Limpo o array em caso de apenas um dado ser retornado para adicionar no array vazio
+                        if (counter == 1) {
+                            singleViewResponseBody.value = gson.fromJson(gson.toJson(response.body()), SingleResponseBody::class.java)
+                            viewArray.add(singleViewResponseBody.value?.responseBody?.records?.viewNames!!)
+                            fragmentViewLoaded.value = true
+                        } else { // Situação de concorrência pode ocorrer quando a chamada é realizada mais de uma vez sem um retorno
+                            if (response.body().toString().contains("situação de concorrência")) {
+                                failureMessage = ""
+                                timerJob?.cancel()
+                                connectionFailure.value = true
+                            }
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                failureMessage = t.toString()
+                connectionFailure.value = true
+            }
+        })
+    }
+
+    // Chamada da procedure e retorno
+    fun procedureCall(procedureBody: ProcedureBody, newUrl: String, cookie: String) {
+        requestRepo.remote.callProcedure(procedureBody, newUrl, cookie).enqueue(object : Callback<Any> {
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                if (response.body().toString().contains("Não autorizado")) { // Erro em caso de tempo limite da chave
+                    connectionStatusDown.value = true
+                } else {
+                    if (response.body().toString().contains("ERRO")) { // Tratamento interno da procedure em caso de erro, mais fácil voltar um ERRO no início
+                        timerJob?.cancel()
+                        failureMessage = response.body().toString()
+                        procedureError.value = true
+                    } else { // Em caso positivo retorno a mensagem da procedure
+                        timerJob?.cancel()
+                        val gson = Gson()
+                        procedureResponse.value = gson.fromJson(gson.toJson(response.body()), ProcedureResponse::class.java)
+                        procedureReturn.value = true
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                timerJob?.cancel()
+                failureMessage = t.toString()
+                connectionFailure.value = true
+            }
+
+        })
+    }
+
+    fun logoutSession(serviceRequest: ServiceRequest, newUrl: String, cookie: String) {
+        requestRepo.remote.logoutSession(serviceRequest, newUrl, cookie).enqueue(object : Callback<Any> {
+            override fun onResponse(call: Call<Any>, response: Response<Any>) {
+                if (response.body().toString().contains("logout")) {
+                    logoutStatus.value = true
+                }
+            }
+
+            override fun onFailure(call: Call<Any>, t: Throwable) {
+                failureMessage = t.toString()
+                connectionFailure.value = true
+            }
+        })
+    }
+
+    // Timer para verificar tempo de resposta
+    fun startTimer() {
+        timerJob = viewModelScope.launch {
+            delay(15000)
+            timerStatus.value = true
+        }
+    }
+
+}
